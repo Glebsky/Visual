9470ac85c00a6023fed039c74a7d547f1b4c8131
unknown <08517@A06_12.zp.itstep.org>
2017-07-23 11:42:20 +0300

    LastCommit

---
+++ SecondSyn/SecondSyn/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SecondSyn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbWork = new System.Windows.Forms.ListBox();
+            this.lbWait = new System.Windows.Forms.ListBox();
+            this.lbCreated = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbWork
+            // 
+            this.lbWork.FormattingEnabled = true;
+            this.lbWork.Location = new System.Drawing.Point(12, 29);
+            this.lbWork.Name = "lbWork";
+            this.lbWork.Size = new System.Drawing.Size(120, 121);
+            this.lbWork.TabIndex = 0;
+            // 
+            // lbWait
+            // 
+            this.lbWait.FormattingEnabled = true;
+            this.lbWait.Location = new System.Drawing.Point(153, 29);
+            this.lbWait.Name = "lbWait";
+            this.lbWait.Size = new System.Drawing.Size(133, 121);
+            this.lbWait.TabIndex = 1;
+            // 
+            // lbCreated
+            // 
+            this.lbCreated.FormattingEnabled = true;
+            this.lbCreated.Location = new System.Drawing.Point(311, 29);
+            this.lbCreated.Name = "lbCreated";
+            this.lbCreated.Size = new System.Drawing.Size(120, 121);
+            this.lbCreated.TabIndex = 2;
+            this.lbCreated.SelectedIndexChanged += new System.EventHandler(this.lbCreated_SelectedIndexChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 161);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(138, 161);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 20);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(311, 161);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(120, 20);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Работающие";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ожидание";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(308, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Созданные";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 193);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.lbCreated);
+            this.Controls.Add(this.lbWait);
+            this.Controls.Add(this.lbWork);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbWork;
+        private System.Windows.Forms.ListBox lbWait;
+        private System.Windows.Forms.ListBox lbCreated;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SecondSyn/SecondSyn/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SecondSyn
+{
+    public partial class Form1 : Form
+    {
+        static int count;
+        Semaphore s = null;
+        List<Thread> threads = new List<Thread>();
+        delegate void ForList(string str);
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnCreate.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Go);
+            count++;
+            t.Name = "Thread" + count.ToString();
+            threads.Add(t);
+            lbCreated.Items.Add(t.Name);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value > 0)
+            {
+                s = new Semaphore(Convert.ToInt32(numericUpDown1.Value), Convert.ToInt32(numericUpDown1.Value));
+                btnCreate.Enabled = true;
+            }
+            else {
+                btnCreate.Enabled = false;
+            }
+        }
+
+        public void Go()
+        {
+            s.WaitOne();
+            Thread ct = Thread.CurrentThread;
+            list2(ct.Name);
+            list3(ct.Name);
+        }
+
+        private void lbCreated_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lbCreated.SelectedIndex;
+            if (index != -1 )
+            {
+                string str = lbCreated.SelectedItem.ToString();
+                str = str.Substring(6);
+                int i = Convert.ToInt32(str);
+                Thread t = threads[i - 1];
+                lbCreated.Items.RemoveAt(index);
+                lbWait.Items.Add(t.Name);
+                t.IsBackground = true;
+                t.Start();
+            }
+        }
+
+        void list2(string tname)
+        {
+            if (lbWait.InvokeRequired)
+            {
+                lbWait.Invoke(new ForList(list2), new object[]{tname});
+                return;
+            }
+            lbWait.Items.Remove(tname);
+        }
+
+        void list3(string tname)
+        {
+            if (lbWork.InvokeRequired)
+            {
+                lbWork.Invoke(new ForList(list3), new object[] { tname });
+                return;
+            }
+            lbWork.Items.Add(tname);
+        }
+    }
+}
